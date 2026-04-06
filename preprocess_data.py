b1b3d53c1e5170aba7f9a1641e47cf455cda1c79
Kev-Park <kevy0403@gmail.com>
2026-04-06 13:44:50 -0400

    add preprocess_data.py

---
+++ preprocess_data.py
@@ -0,0 +1,23 @@
+import pickle
+from pymatgen.core import Structure
+import pandas as pd
+
+def preprocess_data(input_path, output_path):
+
+    df = pd.read_csv(input_path)
+    structures = []
+    targets = []
+
+    for idx, row in df.iterrows():
+        structure = Structure.from_str(row['cif'], fmt='cif')
+        structures.append(structure)
+        targets.append(row['e_above_hull'])
+    
+    data = {'structures': structures, 'targets': targets}
+    with open(output_path, 'wb') as f:
+        pickle.dump(data, f)
+    
+if __name__ == "__main__":
+    input_path = 'raw_data/train.csv'
+    output_path = 'preprocessed_data/train.pkl'
+    preprocess_data(input_path, output_path)
